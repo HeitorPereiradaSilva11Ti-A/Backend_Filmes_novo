e5feb8736363669898cf600804b4e18bdba3f26e
HeitorPereiradaSilva11Ti-A <161168386+HeitorPereiradaSilva11Ti-A@users.noreply.github.com>
2026-09-09 07:30:16 -0300

    Update cadastro.js

--- frontend/cadastro/cadastro.js
+++ frontend/cadastro/cadastro.js
@@ -24,7 +24,7 @@ async function cadastrarFilme() {
         body: JSON.stringify(filme)
     }
 
-    const resposta = await fetch("https://correcao-atv1-3bimestre.vercel.app/create-movie", informacoesAEnviar)
+    const resposta = await fetch("https://backend-filmes-novo.vercel.app/cadastro/cadastro.html/add-movie", informacoesAEnviar)
     const mensagemDecifrada = await resposta.json()
 
     alert(mensagemDecifrada.message)
