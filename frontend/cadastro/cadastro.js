7db705f541aba58755dbd16f1278fafd88b3cc6f
HeitorPereiradaSilva11Ti-A <161168386+HeitorPereiradaSilva11Ti-A@users.noreply.github.com>
2026-09-09 07:27:04 -0300

    Parte de cadastro adicionada

---
+++ frontend/cadastro/cadastro.js
@@ -0,0 +1,33 @@
+async function cadastrarFilme() {
+    const inputTitulo = document.getElementById("titulo")
+    const inputGenero = document.getElementById("genero")
+    const inputClassificacao_Etaria = document.getElementById("classificacao_etaria")
+    const inputDuracao = document.getElementById("duracao")
+
+    if (inputTitulo.value === "" || inputGenero.value === "" || inputClassificacao_Etaria.value === "" || inputDuracao.value === "") {
+        alert("Preencha todas as informações!")
+        return
+    }
+
+    const filme = {
+        title: inputTitulo.value,
+        gender: inputGenero.value,
+        ageLimit: inputClassificacao_Etaria.value,
+        duration: inputDuracao.value
+    }
+
+    const informacoesAEnviar = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(filme)
+    }
+
+    const resposta = await fetch("https://correcao-atv1-3bimestre.vercel.app/create-movie", informacoesAEnviar)
+    const mensagemDecifrada = await resposta.json()
+
+    alert(mensagemDecifrada.message)
+
+    window.location.href = "../index.html"
+}
